864166f50ebca2915ad4263ba57fe6ad51d5b8ec
Paweł Trysła <pawel.trysla@gmail.com>
2019-05-23 21:32:49 +0200

    wip

---
+++ ReactCommon/cxxreact/BundleRegistry.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#import <memory>
+#import "Bundle.h"
+
+namespace facebook {
+namespace react {
+
+class BundleRegistry {
+  public:
+    BundleRegistry() = default;
+    BundleRegistry(const BundleRegistry&) = delete;
+    BundleRegistry& operator=(const BundleRegistry&) = delete;
+    ~BundleRegistry() {};
+
+    void loadBundle(std::string bundlePath, bool inCurrentEnvironment);
+    // JSModulesUnbundle::Module(uint32_t, uint32_t)
+};
+
+} // react
+} // facebook
+
