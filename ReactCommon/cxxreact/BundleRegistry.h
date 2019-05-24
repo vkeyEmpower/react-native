dd5eee2775bdfa6dcd08a64cda25f00af1eaa542
Paweł Trysła <pawel.trysla@gmail.com>
2019-05-24 14:14:12 +0200

    add BundleRegistry header

--- ReactCommon/cxxreact/BundleRegistry.h
+++ ReactCommon/cxxreact/BundleRegistry.h
@@ -1,20 +1,40 @@
 #pragma once
 
-#import <memory>
-#import "Bundle.h"
+#include <memory>
+#include <cxxreact/NativeToJsBridge.h>
+#include "Bundle.h"
+#include "Instance.h"
 
 namespace facebook {
 namespace react {
 
 class BundleRegistry {
   public:
-    BundleRegistry() = default;
+    struct BundleExecutionEnvironment {
+      std::shared_ptr<MessageQueueThread> jsQueue;
+      std::unique_ptr<NativeToJsBridge> nativeToJsBridge;
+      std::weak_ptr<const Bundle> initialBundle;
+    };
+
+    BundleRegistry(JSExecutorFactory* jsExecutorFactory,
+                   std::shared_ptr<ModuleRegistry> moduleRegistry,
+                   std::shared_ptr<InstanceCallback> callback,
+                   std::function<std::shared_ptr<MessageQueueThread>()> jsQueueFactory);
     BundleRegistry(const BundleRegistry&) = delete;
     BundleRegistry& operator=(const BundleRegistry&) = delete;
-    ~BundleRegistry() {};
+    ~BundleRegistry();
 
-    // void loadBundle(std::string bundlePath, bool inCurrentEnvironment);
-    // JSModulesUnbundle::Module(uint32_t, uint32_t)
+    void runNewExecutionEnvironment(std::unique_ptr<const Bundle> initialBundle,
+                                    std::function<void()> callback);
+    void disposeExecutionEnvironments();
+
+  private:
+    std::vector<BundleExecutionEnvironment> bundleExecutionEnvironments_;
+    std::vector<std::shared_ptr<const Bundle>> bundles_;
+    JSExecutorFactory* jsExecutorFactory_;
+    std::shared_ptr<ModuleRegistry> moduleRegistry_;
+    std::shared_ptr<InstanceCallback> callback_;
+    std::function<std::shared_ptr<MessageQueueThread>()> jsQueueFactory_;
 };
 
 } // react
